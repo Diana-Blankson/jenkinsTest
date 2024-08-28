144f8aa55c36e5e791822d2194b662ee185e6a64
Diana Blankson <dblankson.ext@igus.net>
2024-08-28 15:12:09 +0000

    modified two files

--- tests/new.spec.ts
+++ tests/new.spec.ts
@@ -1,9 +1,7 @@
 import {test,expect} from '@playwright/test'
 import { baseUrl } from './data'
 
-test.describe("Api testing", ()=>{
-  test('please', async ({request}) =>{
+  test('Api testing', async ({request}) =>{
    const response = await request.get('https://reqres.in/api/users/2')
    expect(response.status()).toBe(200)
   })
-}) 
--- tests/try.spec.ts
+++ tests/try.spec.ts
@@ -5,8 +5,7 @@ test.describe("Api testing", ()=>{
     test("test", async({request}) =>{
         const response = await request.get(`${baseUrl.URL}api/users/2`)
         const responsebody = JSON.parse(await response.text())
-
-        //assertions
+        
         expect(response.status()).toBe(200)
         expect(responsebody.data.id).toBe(2)
         expect(responsebody.data.first_name).toBe('Janet')
